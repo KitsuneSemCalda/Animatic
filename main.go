b820afc917ea76ef200665a96476816e547a41a2
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-07-23 23:38:42 -0300

    [Feat]: Using prompt UI to collect Data

--- main.go
+++ main.go
@@ -4,6 +4,23 @@ import (
 	"Animatic/config"
 	"fmt"
 	"os"
+  "strings"
+
+  "github.com/manifoldco/promptui"
+)
+
+type model struct {
+	selectedOption optionMsg
+	animeName      string
+}
+
+type optionMsg string
+
+var action string
+
+const (
+	optionDownload optionMsg = "Baixar Anime"
+	optionWatch    optionMsg = "Assistir Anime"
 )
 
 func getFolder() string {
@@ -17,5 +34,38 @@ func getFolder() string {
 }
 
 func main() {
-  loadedConfig, _ := config.LoadConfig(getFolder())
+  config.LoadConfig(getFolder())
+  p := promptui.Select{
+		Label: "Escolha uma opção:",
+		Items: []string{string(optionDownload), string(optionWatch)},
+	}
+
+	_, option, err := p.Run()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	m := model{
+		selectedOption: optionMsg(option),
+	}
+  
+  if (m.selectedOption == optionDownload){
+	  action = "baixado"
+  }else{
+    action = "assistido"
+  }
+
+  prompt := promptui.Prompt{
+		Label: fmt.Sprintf("Digite o nome do anime a ser %s", action),
+	}
+	animeName, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	m.animeName = strings.TrimSpace(animeName)
+  fmt.Println(m.animeName)
+	os.Exit(0)
 }
