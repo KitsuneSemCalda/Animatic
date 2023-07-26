8ff5984b8eac4bec45c86729054712e1181dbce1
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-07-26 11:40:23 -0300

    [Ref]: Clean main.go

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"Animatic/config"
-  "Animatic/downloadAnime"
+  "Animatic/utils"
 	"fmt"
 	"os"
-  "strings"
 
   "github.com/manifoldco/promptui"
 )
@@ -24,62 +23,13 @@ const (
 	optionWatch    optionMsg = "Assistir Anime"
 )
 
-func getFolder() string {
-	userHomeDir, err := os.UserHomeDir()
-	if err != nil {
-		fmt.Println("Erro ao obter o diretório do usuário:", err)
-		return ""
-	}
-
-	return fmt.Sprintf("%s/.local/Animatic", userHomeDir)
-}
 
 func main() {
-  loadedConfig, err := config.LoadConfig(getFolder())
+  loadedConfig, err := config.LoadConfig(utils.GetFolder())
   
   if (err != nil){
     fmt.Printf("Ocorreu um erro: %v\n", err)
     os.Exit(1)
-  }
-
-  p := promptui.Select{
-		Label: "Escolha uma opção:",
-		Items: []string{string(optionDownload), string(optionWatch)},
-	}
-
-	_, option, err := p.Run()
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	m := model{
-		selectedOption: optionMsg(option),
-	}
-  
-  if (m.selectedOption == optionDownload){
-	  p = promptui.Select{
-      Label: "Você pretende baixar um episódio ou todos eles",
-      Items: []string{"Baixar um episódio em especifico", "Baixar todos eles"},
-    }
-    
-    _, option ,err = p.Run()
-
-    if err != nil{
-      fmt.Printf("Error: %v\n", err)
-		  os.Exit(1)
-    }
-      
-    if (option == "Baixar um episódio em especifico"){
-      fmt.Println("Mostrar lista de opções")
-    }
-
-    if (option == "Baixar todos os episódios"){
-      fmt.Println("começar download paralelo")
-    }
-
-  }else{
-    action = "assistido"
   }
 
   prompt := promptui.Prompt{
@@ -91,11 +41,11 @@ func main() {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
-
-	m.animeName = strings.TrimSpace(animeName)
-
-  if (m.selectedOption == optionDownload && loadedConfig.PortugueseSearch()){
-    downloadanime.SearchAnimeSitePtBr(m.animeName)
+  
+  if loadedConfig.DownloadAll(){
+    if loadedConfig.PortugueseSearch() {
+      fmt.Println(animeName)
+    }
   }
 
 	os.Exit(0)
