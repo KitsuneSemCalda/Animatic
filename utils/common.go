85ee2e3c63beb0477e496943ca88fe9fcdc45123
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-07-26 11:39:39 -0300

    [Feat]: Create a common package from utils strings manipulation

---
+++ utils/common.go
@@ -0,0 +1,41 @@
+package utils
+
+import(
+  "regexp"
+  "strings"
+  "path/filepath"
+  "os/user"
+  "log"
+)
+
+func DatabaseFormatter(str string) string {
+	regex := regexp.MustCompile(`\s*\([^)]*\)|\bn/a\b|\s+\d+(\.\d+)?$`)
+	result := regex.ReplaceAllString(str, "")
+	result = strings.TrimSpace(result)
+	result = strings.ToLower(result)
+	return result
+}
+
+func DownloadFolderFormatter(str string) string {
+  regex := regexp.MustCompile(`https:\/\/animefire\.net\/video\/([^\/?]+)`)
+	match := regex.FindStringSubmatch(str)
+	if len(match) > 1 {
+		finalStep := match[1]
+    return finalStep
+  }
+  return ""
+}
+
+func TreatingAnimeName(animeName string) string {
+	loweredName := strings.ToLower(animeName)
+	spacelessName := strings.ReplaceAll(loweredName, " ", "-")
+	return spacelessName
+}
+
+func GetFolder() string{
+  currentUser, err := user.Current()
+	if err != nil {
+		log.Fatalf("Failed to get current user: %v", err)
+	}
+  return filepath.Join(currentUser.HomeDir, "/.local/Animatic")
+}
